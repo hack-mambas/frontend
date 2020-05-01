08803b76c37296ddf0c9116a4690ef9e18aa2c7d
Felipe Maccari <felipe@medplus.com.br>
2020-05-01 16:28:20 -0300

    feat(frontend): add choice buttons

--- src/pages/journey/Choices/Choices.js
+++ src/pages/journey/Choices/Choices.js
@@ -1,10 +1,58 @@
 import React, { memo } from 'react'
-import { Container } from '@material-ui/core'
+
+import { useHistory } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles(() => ({
+  main: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    flexDirection: 'column'
+  },
+  button: {
+    margin: '15px 0',
+    width: '100%'
+  }
+}))
 
 export const Choices = memo(() => {
+  const classes = useStyles()
+
+  const history = useHistory()
+
+  const handleClickProducer = () => {
+    history.push('/journey/producer')
+  }
+
+  const handleClickConsumer = () => {
+    history.push('/journey/consumer')
+  }
+
   return (
     <Container>
-      <div>choices</div>
+      <Box className={classes.main}>
+        <Box>
+          <Typography>Primeiro de tudo, queremos conhecer você.</Typography>
+        </Box>
+
+        <Box display="flex" flexDirection="column">
+          <Button className={classes.button} variant="outlined" color="primary" onClick={handleClickProducer}>
+            Sou Produtor
+          </Button>
+
+          <Button className={classes.button} variant="outlined" color="secondary" onClick={handleClickConsumer}>
+            Sou Consumidor
+          </Button>
+        </Box>
+      </Box>
     </Container>
   )
 })
