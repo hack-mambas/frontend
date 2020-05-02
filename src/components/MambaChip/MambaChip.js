20842039da32863a807be47da9c0bfb9f6e7228e
Felipe Maccari <felipe@medplus.com.br>
2020-05-02 00:48:10 -0300

    feat(frontend): cria component Chip

---
+++ src/components/MambaChip/MambaChip.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import Chip from '@material-ui/core/Chip'
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    backgroundColor: (props) => props.color,
+    color: theme.palette.background.default
+  }
+}))
+
+export const MambaChip = ({ color, label, onClick, ...props }) => {
+  const classes = useStyles({ color })
+
+  return <Chip className={classes.main} onClick={onClick} label={label} {...props} />
+}
---
+++ src/components/MambaChip/index.js
@@ -0,0 +1,1 @@
+export { MambaChip as default } from './MambaChip'
