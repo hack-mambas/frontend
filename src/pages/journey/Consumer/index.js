10918c20b51ce1a53607767e4cb325aed4986360
Felipe Maccari <felipe@medplus.com.br>
2020-05-02 05:24:12 -0300

    wip: listagem produtores

--- src/pages/journey/Consumer/index.js
+++ src/pages/journey/Consumer/index.js
@@ -7,12 +7,16 @@ import LoaderBox from '../../../components/LoaderBox'
 import { routesConsumer } from '../../../constants/routes'
 
 const Marketplace = lazy(() => import('./Marketplace'))
+const Product = lazy(() => import('./Product'))
+const Producer = lazy(() => import('./Producer'))
 
 const Journey = memo(() => {
   return (
     <Suspense fallback={<LoaderBox />}>
       <Layout routes={routesConsumer}>
-        <Route path="/journey/consumer/marketplace" component={Marketplace} />
+        <Route path="/journey/consumer/marketplace" exact component={Marketplace} />
+        <Route path="/journey/consumer/marketplace/product/:id" component={Product} />
+        <Route path="/journey/consumer/marketplace/producer/:id" component={Producer} />
         <Redirect to="/journey/consumer/marketplace" />
       </Layout>
     </Suspense>
