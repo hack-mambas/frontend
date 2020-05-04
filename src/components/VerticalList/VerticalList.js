80042eb40c314a19d52593aecc97ca043b8dc285
Felipe Maccari <felipe@medplus.com.br>
2020-05-03 22:33:39 -0300

    feat(frontend): add vertical list marketplace

---
+++ src/components/VerticalList/VerticalList.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import Box from '@material-ui/core/Box'
+import Icon from '@material-ui/core/Icon'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    display: 'flex',
+    marginBottom: theme.spacing(2),
+
+    '& img': {
+      width: '185px',
+      height: '145px',
+      borderRadius: '10px'
+    }
+  },
+  title: {
+    fontWeight: theme.typography.fontWeightBold
+  },
+  boxInfos: {
+    marginLeft: theme.spacing(2)
+  },
+  badges: {
+    marginTop: theme.spacing(3),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start'
+  },
+  text: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}))
+
+export const VerticalList = ({ items }) => {
+  const classes = useStyles()
+
+  return items.map((item, index) => (
+    <Box key={index} className={classes.main}>
+      <Box>
+        <img alt={item.description} src={item.image} />
+      </Box>
+
+      <Box className={classes.boxInfos}>
+        <Box>
+          <Typography variant="subtitle2" className={classes.title}>
+            {item.description}
+          </Typography>
+          <Typography variant="subtitle2">{item.location}</Typography>
+          <Typography variant="subtitle2">{item.city}</Typography>
+        </Box>
+
+        <Box className={classes.badges}>
+          <Typography variant="subtitle2" className={classes.text}>
+            <Icon color="secondary">emoji_events</Icon> Menores Preços
+          </Typography>
+          <Typography variant="subtitle2" className={classes.text}>
+            <Icon color="primary">two_wheeler</Icon> Entrega hoje
+          </Typography>
+          <Box></Box>
+        </Box>
+      </Box>
+    </Box>
+  ))
+}
--- src/pages/journey/Consumer/Marketplace/Marketplace.js
+++ src/pages/journey/Consumer/Marketplace/Marketplace.js
@@ -1,22 +1,61 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 
-import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Collapse from '@material-ui/core/Collapse'
 import Typography from '@material-ui/core/Typography'
 
+import AddIcon from '@material-ui/icons/Add'
+import MinimizeIcon from '@material-ui/icons/Minimize'
+
 import { MarketplaceFilters } from './MarketplaceFilters'
-import MarketplaceList from '../../../../components/MarketplaceList'
+import VerticalList from '../../../../components/VerticalList'
 
 const useStyles = makeStyles((theme) => ({
   boxes: {
     marginTop: theme.spacing(2)
+  },
+  main: {
+    paddingTop: '10px',
+    display: 'flex',
+    flexDirection: 'row'
   }
 }))
 
+const deliverToday = [
+  {
+    id: '1',
+    description: 'Sítio Aborígene',
+    location: 'Linha Madureira',
+    city: 'Pato Branco, PR',
+    image: 'https://source.unsplash.com/XemjjFd_4qE'
+  },
+  {
+    id: '2',
+    description: 'Morada dos Sonhos',
+    location: 'Com. Independência',
+    city: 'Pato Branco, PR',
+    image: 'https://source.unsplash.com/QcBAZ7VREHQ'
+  }
+]
+
+const favourites = [
+  {
+    id: '1',
+    description: 'Velho Casarão',
+    location: 'Linha Tapera',
+    city: 'Grande Figueira, ST',
+    image: 'https://source.unsplash.com/YvvHEQNgMcU'
+  }
+]
+
 const Marketplace = memo(() => {
   const classes = useStyles()
+
+  const [openedColapse, setOpenedColapse] = useState(true)
+  const [openedColapseFavorites, setOpenedColapseFavorites] = useState(true)
 
   return (
     <Container>
@@ -26,7 +65,35 @@ const Marketplace = memo(() => {
       </Box>
 
       <Box className={classes.boxes}>
-        <MarketplaceList />
+        <Box className={classes.main} onClick={() => setOpenedColapse(!openedColapse)}>
+          <Typography variant="h6">Entregam Hoje</Typography>
+          {openedColapse ? (
+            <MinimizeIcon color="primary" />
+          ) : (
+            <Box marginTop="5px">
+              <AddIcon color="primary" />
+            </Box>
+          )}
+        </Box>
+        <Collapse in={openedColapse} timeout="auto" unmountOnExit>
+          <VerticalList items={deliverToday} />
+        </Collapse>
+      </Box>
+
+      <Box className={classes.boxes}>
+        <Box className={classes.main} onClick={() => setOpenedColapseFavorites(!openedColapseFavorites)}>
+          <Typography variant="h6">Favoritos</Typography>
+          {openedColapseFavorites ? (
+            <MinimizeIcon color="primary" />
+          ) : (
+            <Box marginTop="5px">
+              <AddIcon color="primary" />
+            </Box>
+          )}
+        </Box>
+        <Collapse in={openedColapseFavorites} timeout="auto" unmountOnExit>
+          <VerticalList items={favourites} />
+        </Collapse>
       </Box>
     </Container>
   )
